9918b1147aa4b085616d734c796cbc18a0886f3e
BumbleWeeb <Alex@abreu.net>
2026-05-09 13:04:27 -0400

    Created Precision_score function

--- SER_Project/minilearn/metrics.py
+++ SER_Project/minilearn/metrics.py
@@ -17,3 +17,18 @@ def confusion_matrix(y_true, y_pred):
     for true, pred in zip(y_true, y_pred):
         cm[class_to_idx[true]][class_to_idx[pred]] +=1
     return cm, classes
+
+def precision_score(y_true, y_pred, average='macro'):
+    cm, classes = confusion_matrix(y_true, y_pred)
+
+    precisions = []
+    for i in range(len(classes)):
+        #true postives for this class
+        tp=cm[i,i]
+        #all predicted as this class
+        fp = np.sum(cm[:, i]) - tp
+        precision = tp/ (tp+fp) if (tp+fp) > 0 else 0
+        precisions.append(precision)
+    if average == 'macro':
+        return np.mean(precisions)
+    return np.array(precisions)
