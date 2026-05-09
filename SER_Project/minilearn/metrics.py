a3de75e805f8b89eb464a493e98046cc3e4ef37b
BumbleWeeb <Alex@abreu.net>
2026-05-09 13:01:01 -0400

    Finished Confusion matrix function

--- SER_Project/minilearn/metrics.py
+++ SER_Project/minilearn/metrics.py
@@ -7,5 +7,13 @@ def accuracy_score(y_true, y_pred):
 def confusion_matrix(y_true, y_pred):
     #get all unique classes in sorted order
     classes = np.unique(np.concatenate([y_true, y_pred]))
-    b_classes = len(classes)
+    n_classes = len(classes)
 
+    #create a mapping from class label to index
+    class_to_idx = {cls: idx for idx, cls in enumerate(classes)}
+
+    #building confusion matrix
+    cm= np.zeros((n_classes, n_classes), dtpye=int)
+    for true, pred in zip(y_true, y_pred):
+        cm[class_to_idx[true]][class_to_idx[pred]] +=1
+    return cm, classes
