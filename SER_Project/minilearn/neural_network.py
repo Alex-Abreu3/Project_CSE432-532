deb760f9720e471078f127654d30ec3317b8ca3e
BumbleWeeb <Alex@abreu.net>
2026-05-10 16:08:32 -0400

    Created Init function for ANN

--- SER_Project/minilearn/neural_network.py
+++ SER_Project/minilearn/neural_network.py
@@ -1,2 +1,15 @@
 import numpy as np
 
+class ANN:
+    def __init__(self, hidden_size=128, learning_rate=0.01, n_iterations=1000):
+        self.hidden_size = hidden_size
+        self.learning_rate = learning_rate
+        self.n_iterations = n_iterations
+        self.weights1 = None
+        self.weights2 = None
+        self.bias1 = None
+        self.bias2 = None
+        self.classes_ = None
+        self.loss_history =[]
+    
+    
